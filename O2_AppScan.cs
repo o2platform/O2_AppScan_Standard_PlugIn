1d6f35cf6a952e20c308d459063e674f977b520f
DinisCruz <dinis.cruz@owasp.org>
2012-06-25 18:29:25 +0100

    First Commit

---
+++ O2_AppScan.cs
@@ -0,0 +1,45 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Windows.Forms;
+    using AppScan;
+    using AppScan.Scan;
+    using AppScan.Events;
+    using AppScan.Extensions;
+    using AppScan.Scan.Events;
+    using O2.DotNetWrappers.ExtensionMethods;
+    using O2.XRules.Database.Utils;
+
+    namespace O2.APIs
+    {
+        public class O2_AppScan_Standard_PlugIn : IExtensionLogic
+        {
+            public static IAppScan      AppScan          { get; set; }
+            public static IAppScanGui   AppScanGui       { get; set; }
+            public static string        ExtensionDir    { get; set; }
+
+            public void Load (IAppScan appscan, IAppScanGui appScanGui, string extensionDir)
+            {            
+                "AppScan ExtensionDir: {0}".info(extensionDir);
+            
+                AppScan = appscan;                        
+                AppScanGui = appScanGui;
+                ExtensionDir = extensionDir;
+                addO2MenuItems();
+            }
+
+            public void addO2MenuItems()
+            {
+                var menuItem_LogViewer= new AppScan.Extensions.MenuItem<EventArgs>("O2 LogViewer", (sender)=> "O2 LogViewer".popupWindow().add_LogViewer());            
+                AppScanGui.ExtensionsMenu.Add(menuItem_LogViewer);
+
+                var menuItem_ScriptEditor = new AppScan.Extensions.MenuItem<EventArgs>("O2 C# REPL Editor", (sender)=> ascx_Panel_With_Inspector.runControl());            
+                AppScanGui.ExtensionsMenu.Add(menuItem_ScriptEditor);                        
+            }
+            
+            public ExtensionVersionInfo GetUpdateData(Edition targetApp, Version targetAppVersion)
+            {
+                    return null;
+            }
+          }
+
+    }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("O2_AppScan_Standard_PlugIn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("O2_AppScan_Standard_PlugIn")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("959fb06a-7c2c-4306-a5a7-06c5e613b706")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
